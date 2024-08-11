2bd7e0c5a6832ace7e2374ad74ec7d5a23ff07e9
shreyashbijlwanOP <shreyash.bijlwanmail@gmail.com>
2024-08-11 08:57:44 +0530

    added react-dnd-beautiful

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+import dummyData from "../users.json";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+
 function App() {
-  return <center>This is React App</center>;
+  const [users, setUsers] = useState(dummyData.users);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const updatedUsers = reorder(
+      users,
+      result.source.index,
+      result.destination.index
+    );
+
+    setUsers(updatedUsers);
+  };
+  return (
+    <div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided:any) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {users.map((item, index) => (
+                <Draggable key={item.id} draggableId={index} index={index}>
+                  {(provided:any) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {item.firstName}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 }
 
 export default App;
